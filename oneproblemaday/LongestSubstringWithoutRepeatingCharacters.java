1d1c2a073b939de3c130fa8bef265bc11f4cf697
Saniyat Al Ahmed <saniyat013@gmail.com>
2022-09-08 10:49:44 +0600

    One Problem a Day - Day 3 Problem: #3, Longest Substring Without Repeating Characters

---
+++ oneproblemaday/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,29 @@
+package problems.oneproblemaday;
+
+import java.util.HashSet;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+	public static int lengthOfLongestSubstring(String s) {
+		HashSet<Character> charSet = new HashSet<>();
+		int maxSubString = 0, i = 0, j = 0;
+
+		while (j < s.length()) {
+			if (charSet.add(s.charAt(j))) {
+				maxSubString = Math.max(maxSubString, charSet.size());
+				j++;
+			} else {
+				charSet.remove(s.charAt(i++));
+			}
+		}
+
+		return maxSubString;
+	}
+
+	public static void main(String[] args) {
+		String s = "  ";
+//		String s = "pwwkew";
+//		String s = "dvdf";
+//		String s = "abcddefgh";
+		System.out.printf("RESULT: %d", lengthOfLongestSubstring(s));
+	}
+}
